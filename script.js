4f3dbfd75a863b7ddc88a32247c995195743ea7f
AndrewDongAndy <dongandrew99@gmail.com>
2018-11-29 20:20:14 -0500

    Reorganized functions and began work on canvas

--- script.js
+++ script.js
@@ -6,30 +6,32 @@ December 21, 2018
 
 Javascript Programming Challenge
 
-A program that allows the user to play a game of Hangman.
+A program that allows the user to play Hangman.
 
-TODO add calculations, graphics, and animation (as required
+TODO add graphics, and animation (as required
 by the evaluation criteria)
 
 QUESTION: does generating a random number count as a calculation?
+ANSWER: yes
 */
 
 // ----------STATIC----------
 
-
-const MAX_ANS_LEN = 30;
+const MAX_ANS_LEN = 30; // longest answer allowed
 
 var answers; // master array of all possible answers
 
 // HTML elements for easier reference in code
 var btnGuess = document.getElementById('btnGuess');
 var btnNewGame = document.getElementById('btnNewGame');
-var txtGuess = document.getElementById('txtGuess');
-var txtCustomAns = document.getElementById('txtCustomAns');
+var canvasHangman = document.getElementById('canvasHangman'); // not implemented
+var divGuessing = document.getElementById('divGuessing');
+var divStartGame = document.getElementById('divStartGame');
 var preCurState = document.getElementById('preCurState');
+var txtGuess = document.getElementById('txtGuess');
 
 // ASCII id for the character 'A'
-const idA = 65;
+const ID_A = 65;
 
 // Returns whether or not the given character is alphabetic.
 function isAlpha(c) {
@@ -39,20 +41,18 @@ function isAlpha(c) {
 // Returns a random integer in the range [a, b].
 function randInt(a, b) {
     return a + Math.floor(Math.random() * (b - a + 1));
-    // QUESTION: does this count as a calculation?
-    // ANSWER: yes
 }
 
 // ----------END STATIC----------
 
 
 var guesses; // guesses the user has taken
-var correct; // guesses the user has taken which were characters in the answer
+var incorrect; // guesses the user has taken which were not characters in the answer
 var answer;
 var len;
 
 var charGuessed; // whether each character has been guessed
-var charLocations; // locations of each letter
+var charLocations; // locations of each letter; 2D array
 var curState; // current state of the game
 
 
@@ -65,7 +65,67 @@ function setAnswer(ans) {
     console.assert(isValidAnswer(ans), 'invalid answer!!!');
     answer = ans;
     len = answer.length;
+    initCharGuessed();
+    initCharLocations();
+    initCurState();
+    updateState();
 }
+
+// Sets a random answer for a new regular game.
+function setRandomAnswer() {
+    setAnswer(answers[randInt(0, answers.length - 1)]);
+}
+
+
+// ----------UTILITY FUNCTIONS----------
+
+// Returns whether a string is a valid answer string:
+// each character must be alphabetic or a space.
+function isValidAnswer(s) {
+    if (s.length == 0 || s.length > MAX_ANS_LEN) return false;
+    for (let i = 0; i < s.length; i++) {
+        let c = s.charAt(i);
+        if (!isAlpha(c) && c != ' ') return false;
+    }
+    return true;
+}
+
+// Returns normalized string (all uppercase with single spaces
+// between words)
+function normalized(s) {
+    s = s.trim();
+    let res = '';
+    for (let i = 0; i < s.length; i++) {
+        // below: works due to short-circuiting of || operator
+        if (s.charAt(i) != ' ' || res.charAt(res.length - 1) != ' ')
+            res += s.charAt(i);
+    }
+    return res.toUpperCase();
+}
+
+// Returns current state of game (for display only)
+function getCurState() {
+    var s = '';
+    for (let i = 0; i < len; i++) {
+        if (curState[i] == ' ') s += '  '; // exaggerate spaces in word
+        else s += curState[i];
+        s += ' ';
+    }
+    return s;
+}
+
+// Returns whether user has won the game; the user wins
+// if and only if the current state does not contain any
+// underscores
+function hasWon() {
+    for (let i = 0; i < len; i++) {
+        if (curState[i] == '_') return false;
+    }
+    return true;
+}
+
+// ----------END UTILITY FUNCTIONS----------
+
 
 // ----------GAME INITIALIZATION----------
 
@@ -117,11 +177,9 @@ function initCharLocations() {
     for (let i = 0; i < 26; i++) {
         charLocations[i] = [];
     }
-
-    // Store indices containing each letter
     for (let i = 0; i < len; i++) {
         if (answer.charAt(i) == ' ') continue; // ignore spaces
-        let v = answer.charCodeAt(i) - idA; // position in alphabet
+        let v = answer.charCodeAt(i) - ID_A; // position in alphabet
         charLocations[v].push(i);
     }
 }
@@ -135,58 +193,30 @@ function initCurState() {
     }
 }
 
-// Returns whether a string is a valid answer string:
-// each character must be alphabetic or a space.
-function isValidAnswer(s) {
-    if (s.length == 0 || s.length > MAX_ANS_LEN) return false;
-    for (let i = 0; i < s.length; i++) {
-        let c = s.charAt(i);
-        if (!isAlpha(c) && c != ' ') return false;
-    }
-    return true;
-}
-
-// Returns normalized string (all uppercase, single spaces
-// between words)
-function normalized(s) {
-    s = s.trim();
-    let res = '';
-    for (let i = 0; i < s.length; i++) {
-        // below: works with short-circuiting of || operator
-        if (s.charAt(i) != ' ' || res.charAt(res.length - 1) != ' ')
-            res += s.charAt(i);
-    }
-    return res.toUpperCase();
-}
-
-// Loads the answer for a game.
-function loadAnswer() {
-    let s = txtCustomAns.value;
-    s = normalized(s);
-    if (!isValidAnswer(s)) {
-        setAnswer(answers[randInt(0, answers.length - 1)]);
-        if (s.length != 0) { // user tried to use custom word
-            alert('Invalid custom word: random word chosen from word bank.');
-        }
-    }
-    else {
-        setAnswer(s);
-    }
-}
-
-// Called when user clicks the 'New game' button
+// Called when user clicks btnNewGame
 function newGame() {
     guesses = 0;
-    correct = 0;
-    initAnswers();
-    loadAnswer();
-    initCharGuessed();
-    initCharLocations();
-    initCurState();
-    updateState();
-    btnGuess.disabled = false;
-    txtCustomAns.value = '';
+    incorrect = 0;
+    setRandomAnswer();
+    divGuessing.hidden = false;
     txtGuess.focus();
+}
+
+// Called when user clicks btnCustomGame
+function customGame() {
+    guesses = 0;
+    incorrect = 0;
+    let ans = prompt(`Enter an answer string; alphabetic characters only, `
+        + `maximum ${MAX_ANS_LEN} characters.`);
+    ans = normalized(ans);
+    if (!isValidAnswer(ans)) {
+        alert('Sorry, that is not a valid answer string.');
+        return;
+    }
+    setAnswer(ans);
+    divGuessing.hidden = false;
+    txtGuess.focus();
+    alert(`Success! Answer set as: ${answer}`);
 }
 
 // ----------END GAME INITIALIZATION----------
@@ -195,42 +225,22 @@ function newGame() {
 
 // ----------USER INTERACTION----------
 
-// Returns whether user has won the game; the user wins
-// if and only if the current state does not contain any
-// underscores
-function hasWon() {
-    for (let i = 0; i < len; i++) {
-        if (curState[i] == '_') return false;
-    }
-    return true;
-}
-
 // Processes guessing a character; changes the values
 // of the variables and handles bad input.
 function guessChar(c) {
     if (!isGuessValid(c)) return;
     c = c.toUpperCase();
-    let v = c.charCodeAt(0) - idA;
+    let v = c.charCodeAt(0) - ID_A;
     guesses++;
     charGuessed[v] = true;
     // show this character where it appears in the answer
     charLocations[v].forEach(function(i) {
         curState[i] = c;
     });
-    if (charLocations[v].length > 0) {
-        correct++;
+    if (charLocations[v].length == 0) {
+        incorrect++;
+        drawBodyPart();
     }
-}
-
-// Returns current state of game (for display only)
-function getCurState() {
-    var s = '';
-    for (let i = 0; i < len; i++) {
-        if (curState[i] == ' ') s += '  '; // exaggerate spaces in word
-        else s += curState[i];
-        s += ' ';
-    }
-    return s;
 }
 
 // Returns whether the given string is a valid guess
@@ -243,7 +253,7 @@ function isGuessValid(c) {
     }
     c = c.toUpperCase();
     // check if this character has been guessed before
-    let v = c.charCodeAt(0) - idA;
+    let v = c.charCodeAt(0) - ID_A;
     if (charGuessed[v]) {
         alert(`You have already guessed ${c}; please try again.`);
         return false;
@@ -255,7 +265,6 @@ function isGuessValid(c) {
 // This includes letters/underscores, guesses used,
 // incorrect guesses, and whether user has won.
 function updateState() {
-    let incorrect = guesses - correct;
     let s = `Current state:<br>${getCurState()}<br>`;
     s += `You have used ${guesses} `
         + (guesses == 1 ? 'guess' : 'guesses') + ' so far.<br>';
@@ -263,9 +272,28 @@ function updateState() {
         + (incorrect == 1 ? 'time' : 'times') + '.';
     if (hasWon()) {
         s += '<br>Congratulations! You won!';
-        btnGuess.disabled = true;
+        divGuessing.hidden = true;
     }
     preCurState.innerHTML = s;
+}
+
+// Draws the body part corresponding to the number of
+// incorrect guesses so far
+function drawBodyPart() {
+    // TODO: get HTML canvas working
+    if (incorrect == 1) {
+
+    }
+}
+
+
+// ----------EVENT HANDLING----------
+
+// Checks for when user presses enter to guess a character
+function txtGuessKeyPressed(event) {
+    if (event.charCode == 13) { // enter key
+        guess();
+    }
 }
 
 // Called by btnGuess to guess a character
@@ -276,3 +304,8 @@ function guess() {
     txtGuess.value = '';
     txtGuess.focus();
 }
+
+// ----------END EVENT HANDLING----------
+
+
+initAnswers();
